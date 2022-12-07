2320bb266d12ecf3999dd10d8d0f93a232b45782
mahyar sherafat <mahyar sherafat>
2022-12-07 21:35:07 +0330

    create schemas

---
+++ app/models/Project.js
@@ -0,0 +1,17 @@
+
+const  mongoose = require('mongoose');
+
+const ProjectSchema=new mongoose.Schema({
+    title:{type:String,required:true},
+    description:{type:String},
+    image:{type:string,default:"/defaults/project.png"},
+    owner:{type:mongoose.Types.ObjectId,required:true},
+    team:{type:mongoose.Types.ObjectId},
+    private:{type:Boolean,required:true},
+},{
+    timestamps:true
+});
+const ProjectModel=mongoose.model("project",ProjectSchema);
+module.exports={
+    ProjectModel
+}

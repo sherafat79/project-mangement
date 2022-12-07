8f177d7627bf203fdebdd5eb2428fb0ee0fb5b43
mahyar sherafat <mahyar sherafat>
2022-12-07 22:05:18 +0330

    config project

---
+++ app/http/controllers/User.controller.js
@@ -0,0 +1,32 @@
+const { Controller } = require("./Controller");
+
+class UserController extends Controller{
+    getAllUsers(){
+        
+    }
+    showUserById(){
+
+    }
+    updateUser(){
+
+    }
+    deleteUser(){
+
+    }
+    addSkills(){
+
+    }
+    editSkills(){
+        
+    }
+    
+    acceptInviteInTeam(){
+
+    }
+    rwjectInviteInTeam(){
+        
+    }
+}
+module.exports={
+    UserController :new UserController()
+}
